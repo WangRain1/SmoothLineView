c44b88bc94b9ebbc297d07f320c831b884286a04
fengyou0822 <fengyou0822@thundersoft.com>
2019-05-13 17:05:09 +0800

    action move points

--- app/src/main/java/com/example/ts/smoothlineview/SmoothLineView.java
+++ app/src/main/java/com/example/ts/smoothlineview/SmoothLineView.java
@@ -12,6 +12,7 @@ import android.graphics.Path;
 import android.graphics.PointF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -48,77 +49,128 @@ public class SmoothLineView extends View {
         mPoints = initPoint();
     }
 
-
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         List<PointF> fList = getControlPoints(mPoints);
         mPath.reset();
-        for (int i=0;i<mPoints.length-1;i++){
-            mPath.moveTo(mPoints[i].x,mPoints[i].y);
+        mPaint.setColor(Color.BLUE);
+        for (int i = 0; i < mPoints.length - 1; i++) {
+            mPath.moveTo(mPoints[i].x, mPoints[i].y);
             mPath.cubicTo(
-                    fList.get(i*2).x,fList.get(i*2).y,
-                    fList.get(i*2+1).x,fList.get(i*2+1).y,
-                    mPoints[i+1].x,mPoints[i+1].y
+                    fList.get(i * 2).x, fList.get(i * 2).y,
+                    fList.get(i * 2 + 1).x, fList.get(i * 2 + 1).y,
+                    mPoints[i + 1].x, mPoints[i + 1].y
             );
         }
-        canvas.drawPath(mPath,mPaint);
-        for (PointF pointF : mPoints){
-
-            canvas.drawCircle(pointF.x,pointF.y,2,mPaint);
+        canvas.drawPath(mPath, mPaint);
+        for (PointF pointF : mPoints) {
+            canvas.drawCircle(pointF.x, pointF.y, 5, mPaint);
         }
-        for (PointF pointF : fList){
+        for (PointF pointF : fList) {
             mPaint.setColor(Color.GREEN);
-            canvas.drawCircle(pointF.x,pointF.y,2,mPaint);
+            canvas.drawCircle(pointF.x, pointF.y, 2, mPaint);
+        }
+        mPaint.setColor(Color.RED);
+        canvas.drawLine(mPoints[0].x, mPoints[0].y, mPoints[2].x, mPoints[2].y, mPaint);
+
+        canvas.drawLine(fList.get(1).x, fList.get(1).y, fList.get(2).x, fList.get(2).y, mPaint);
+    }
+
+    float x , y;
+    int index;
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        int a = event.getAction();
+        switch (a) {
+            case MotionEvent.ACTION_DOWN:
+                x = event.getX();
+                y = event.getY();
+                if (!checkClickPosition(x,y)){
+                    return false;
+                }
+
+        }
+        return super.dispatchTouchEvent(event);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int a = event.getAction();
+        switch (a) {
+            case MotionEvent.ACTION_DOWN:
+//                x = event.getX();
+//                y = event.getY();
+//                if (!checkClickPosition(x,y)){
+//                    return false;
+//                }
+            case MotionEvent.ACTION_MOVE:
+                mPoints[index].y = event.getY();
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
         }
 
+        return true;
+    }
+
+    private boolean checkClickPosition(float x,float y){
+        for (int i =0;i<mPoints.length;i++){
+            if ((x < mPoints[i].x +20 && x >mPoints[i].x-20) && (y < mPoints[i].y +20 && y > mPoints[i].y-20)){
+                index = i;
+                return true;
+            }
+        }
+        return false;
     }
 
     private PointF[] initPoint() {
         return mPoints = new PointF[]{
-                new PointF(0,200),
-                new PointF(50,100),
-                new PointF(100,150),
-                new PointF(150,100),
-                new PointF(200,180),
-                new PointF(230,300),
-                new PointF(260,80),
-                new PointF(300,500),
-                new PointF(340,180),
-                new PointF(400,130),
-                new PointF(450,480),
+                new PointF(0, 200),
+                new PointF(50, 100),
+                new PointF(100, 150),
+                new PointF(150, 100),
+                new PointF(200, 180),
+                new PointF(230, 300),
+                new PointF(260, 80),
+                new PointF(300, 500),
+                new PointF(340, 180),
+                new PointF(400, 130),
+                new PointF(450, 480),
         };
     }
 
-    private List<PointF> getControlPoints(PointF[] points){
+    private List<PointF> getControlPoints(PointF[] points) {
         // 计算斜率 y = kx + b => k = (y - b)/x
-        if (points.length < 3){
+        if (points.length < 3) {
             return null;
         }
         List<PointF> pointFList = new ArrayList<>();
-        float rate = 0.4f;
+        float rate = 0.7f;
         //从第一个控制点开始计算
-        float cx1 = points[0].x + (points[1].x - points[0].x)*rate;
+        float cx1 = points[0].x + (points[1].x - points[0].x) * rate;
         float cy1 = points[0].y;
-        pointFList.add(new PointF(cx1,cy1));
+        pointFList.add(new PointF(cx1, cy1));
 
-        for (int i =1;i<points.length-1;i++){
+        for (int i = 1; i < points.length - 1; i++) {
             //第二个点
-            float k = (points[i+1].y - points[i-1].y)/(points[i+1].x - points[i-1].x);
-            float b = points[i].y - k*points[i].x;
+            float k = (points[i + 1].y - points[i - 1].y) / (points[i + 1].x - points[i - 1].x);
+            float b = points[i].y - k * points[i].x;
             //左边控制点
-            float cxLeft = points[i].x - (points[i].x - points[i-1].x)*rate;
-            float cyLeft = k*cxLeft + b;
-            pointFList.add(new PointF(cxLeft,cyLeft));
+            float cxLeft = points[i].x - (points[i].x - points[i - 1].x) * rate;
+            float cyLeft = k * cxLeft + b;
+            pointFList.add(new PointF(cxLeft, cyLeft));
             //右边控制点
-            float cxRight = points[i].x + (points[i+1].x - points[i].x)*rate;
-            float cyRight = k*cxRight + b;
-            pointFList.add(new PointF(cxRight,cyRight));
+            float cxRight = points[i].x + (points[i + 1].x - points[i].x) * rate;
+            float cyRight = k * cxRight + b;
+            pointFList.add(new PointF(cxRight, cyRight));
         }
         //最后一个点
-        float cxLast = points[points.length - 1].x - (points[points.length - 1].x - points[points.length - 2].x)*rate;
+        float cxLast = points[points.length - 1].x - (points[points.length - 1].x - points[points.length - 2].x) * rate;
         float cyLast = points[points.length - 1].y;
-        pointFList.add(new PointF(cxLast,cyLast));
+        pointFList.add(new PointF(cxLast, cyLast));
         return pointFList;
     }
 
